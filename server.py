cb8484ff71236904756aa93fc14985390ae6249f
Tarun Mittal <tm6622@g.rit.edu>
2023-05-16 08:37:29 -0400

    added submit_form route

--- server.py
+++ server.py
@@ -9,3 +9,7 @@ def my_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submitted hooraayyy'
