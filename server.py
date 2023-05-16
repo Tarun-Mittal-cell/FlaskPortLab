5484be0127c0e6d4cbbd4a3afb7e2cf76f939675
Tarun Mittal <tm6622@g.rit.edu>
2023-05-16 06:34:47 -0400

    serving html files using render_template

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello!!!!!'
+    return render_template('./index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('./about.html')
 
 @app.route('/blog')
 def blog():
     return 'These are my thoughts on blogs'
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'this is my dog'
